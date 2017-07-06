c48f052fd69379d7fead87dd11da93b9d19872e5
Michael Erwin Virgines <mike@rtb.cat>
2017-07-07 01:41:54 +0800

    Server date query added.

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.get('/info/inventory',function(req,res){
     MongoClient.connect('mongodb://localhost/apnx', search);
+
     function search(error,db){
+        let date_today       = (new Date()).toISOString().replace(/T\d{2}.+/,"");
+        let scrape_last_file = path.join(__dirname,'logs','scrape_last.log');
+        if(!fs.existsSync(scrape_last_file)) fs.writeFileSync(scrape_last_file, date_today);
+        let scrape_last      = fs.readFileSync(scrape_last_file).toString();
+
         let collection  = db.collection('inventory');
         let geo_country = !req.query.country? 'any' : (req.query.country == 'all'? 'any' : req.query.country);
         let device_type = !req.query.device_type? "any" : req.query.device_type;
         let supply_type = !req.query.supply_type? "any" : req.query.supply_type;
         let keywords = !req.query.keyword? "" : req.query.keyword;
-        let find = {"filter": `${geo_country}.${device_type}.${supply_type}`};
-        if(keywords.length > 0) find.seller_member_name = {$regex: keywords, $options: "i"};
         let sort = !req.query.sort_by? "filtered_imps" : req.query.sort_by;
         let order = !req.query.order_by? -1 : (req.query.order_by == "asc"? 1 : -1);
         let limit = !req.query.limit? 25 : Number(req.query.limit);
         let pages = 0;
         let page  = !req.query.page? 1 : Number(req.query.page);
         let skip  = limit * (page - 1);
-        let sorting = {};
-        sorting[sort] = order;
-
-        collection.find(find).count((error,data)=>{
+        let sorting = {}; sorting[sort] = order;
+        let date = !req.query.date? new Date(scrape_last) : (/\d{4}\-\d{2}\-\d{2}/.test(req.query.date)? new Date(req.query.date) : new Date(scrape_last))
+        let find = {
+            "filter": `${geo_country}.${device_type}.${supply_type}`,
+            "scrape_date": date
+        };
+        if(keywords.length > 0) find.seller_member_name = {$regex: keywords, $options: "i"};
+        collection.count(find,(error,data)=>{
             pages = Math.ceil(data / limit);
             collection.find(find).limit(limit).skip(skip).sort(sorting).toArray((error,docs)=>{
                 let response = {
@@ -36,12 +46,8 @@ app.get('/info/inventory',function(req,res){
                 res.json(response);
                 db.close();
             });
-            // res.send("records: "+data);
-            // db.close();
         });
     }
-    // console.log(req.query.country);
-    // res.json(req.params);
 });
 
 app.listen(8000,function(){
